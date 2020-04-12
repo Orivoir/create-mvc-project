c5cc9efb5d1a823a34b3ee1587b55c9ccd9641e4
Samuel Gaborieau <sam.gabor@hotmail.com>
2020-04-12 08:27:38 +0200

    upgrade router

---
+++ test/build-routes.test.js
@@ -0,0 +1,35 @@
+const
+    {assert,expect} = require('chai'),
+    pathResolver = require('path') ,
+
+    ClassAnnotations = require('class-annotations')(
+        pathResolver.join( __dirname,'./factory-data/files/' )
+    ) ,
+    factoryData = require('./factory-data/build-routes.json') ,
+
+    fs = require('fs') ,
+
+    buildRoutes = require('./../lib/build-files/content-files/build-routes')
+;
+
+describe('`build-routes` function start', () => {
+
+    it('should be a function', () => {
+
+        assert.isFunction( buildRoutes ) ;
+
+    } ) ;
+
+    it('should accept only `object` as arg1', () => {
+
+        const fxThrow = () => buildRoutes( null ) ;
+
+        const fxNotThrow = () => buildRoutes( {} ) ;
+
+        expect( fxThrow ).to.be.throw( RangeError, "arg1: annotations controller should be a object" ) ;
+
+        expect( fxNotThrow ).to.be.not.throw( RangeError ) ;
+
+    } ) ;
+
+} ) ;
---
+++ test/factory-data/files/controller/class-0.js
@@ -0,0 +1,41 @@
+/**
+ * @Controller({
+ *      path: "/main"
+ * })
+ */
+class Main {
+
+    /**
+     * should be exclude value
+     *
+     * @TODO get milk
+     *
+     * @Route( {
+     *      name: "index",
+     *      methods: ["GET"] ,
+     *      path: "/"
+     * } )
+     * @eg 55
+     */
+    bar() {
+
+        // ...
+    }
+
+    /**
+     * should be exclude value
+     *  @eg 55
+     * @TODO get milk
+     *
+     * @Route( {
+     *      name: "index",
+     *      methods: ["GET"] ,
+     *      path: "/abc"
+     * } )
+     */
+    ijkdef() {
+
+        // ...
+    }
+
+} ;
